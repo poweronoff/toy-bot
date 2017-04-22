6c9740bf4ec0315d03bb331221f6f65301709878
poweronoff <dj.steam@gmail.com>
2017-04-22 21:46:05 +0200

    move strategy implemented

--- src/main/java/de/dj_steam/bot/domain/ToyBotField.java
+++ src/main/java/de/dj_steam/bot/domain/ToyBotField.java
@@ -9,4 +9,12 @@ import lombok.Getter;
 public class ToyBotField {
     private int width = 4;
     private int height = 4;
+
+    public boolean isInsideTheField(final Position position) {
+        return (position.getX() >= 0 &&
+                position.getY() >= 0 &&
+                position.getX() <= width &&
+                position.getY() <= height);
+    }
+
 }
---
+++ src/test/java/de/dj_steam/bot/domain/ToyBotFieldTest.java
@@ -0,0 +1,26 @@
+package de.dj_steam.bot.domain;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * @author steam
+ */
+public class ToyBotFieldTest {
+    @Test
+    public void isInsideTheField() throws Exception {
+        ToyBotField toyBotField = new ToyBotField();
+        assertTrue(toyBotField.isInsideTheField(new Position(0,0)));
+        assertTrue(toyBotField.isInsideTheField(new Position(4,4)));
+
+        assertFalse(toyBotField.isInsideTheField(new Position( -1,-1)));
+        assertFalse(toyBotField.isInsideTheField(new Position(5,5)));
+
+        assertFalse(toyBotField.isInsideTheField( new Position(-1,4)));
+        assertFalse(toyBotField.isInsideTheField( new Position(1,5)));
+
+    }
+
+}
